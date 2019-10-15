f61bc850f991cc52ae1b63339b1649f58cf92ceb
Student <Student@304-1>
2019-10-15 21:07:40 +0300

    Krestiki_Noliki

--- XO/main.cpp
+++ XO/main.cpp
@@ -2,17 +2,20 @@
 #include<conio.h>
 #define HORIZ_OFFSET "\t\t\t\t\t\t"
 using namespace std;
-void PrintField(char Field[], const int n);
-void Move(char Field[], const int n);
+void PrintField(char Field[], const int n, char Player);
+void Move(char Field[], const int n, char Player);
+void Check(char Field[], const int n, char Player);
 
 void main()
 {
 	setlocale(LC_ALL, "Rus");
-	const int SIZE = 9+1;
+	const int SIZE = 9 + 1;
 	char Field[SIZE] = {}; //Игровое поле 
-	PrintField(Field, SIZE);
+	PrintField(Field, SIZE, '0');
+	cout << "Еще разочек? (y || Y)";
+	if (_getch() == 'y')main();
 }
-void PrintField(char Field[], const int n)
+void PrintField(char Field[], const int n, char Player)
 {
 	system("CLS");
 	cout << "\n\n\n\n\n\n\n";
@@ -23,20 +26,71 @@ void PrintField(char Field[], const int n)
 		{
 			if (j == 0)cout << " ";
 			cout << Field[i + j];
-			if(j!=2) cout << " | ";
+			if (j != 2) cout << " | ";
 		}
 		cout << endl;
 		cout << HORIZ_OFFSET;
-		if(i!=0) cout << "--- --- ---"<< endl;
+		if (i != 0) cout << "--- --- ---" << endl;
 	}
-	Move(Field, n);
+	Check(Field, n, Player);
 }
-void Move(char Field[], const int n)
+void Move(char Field[], const int n, char Player)
 {
-		char key;
+	char key;
+	//cout << (int)key << endl
+	bool busy;
+	bool miss;
+	/*do
+	{
+		busy = false;
 		key = _getch();
 		if (key == 27)return;
-		//cout << (int)key << endl;
-		Field[key - 49] = key;
-		PrintField(Field, n);
+		if (Field[key - 49] != 0)busy = true;
+		else Field[key - 49] = Player;
+		if (busy)cout << " Клетка занята" << endl;
+	} while (busy);*/
+	do
+	{
+		busy = false;
+		do
+		{
+			miss = false;
+			key = _getch();
+		    if (key == 27)return;
+			if (key < '1' || key > '9')miss = true;
+			if (miss) cout << "Вы не попали в поле\a" << endl;
+		} while (miss);
+
+		if (Field[key - 49] == 0)Field[key - 49] = Player;
+		else
+		{
+			cout << "Cell busy\a" << endl;
+			busy = true;
+		}
+	} while (busy);
+	/*if (Player == 'X')PrintField(Field, n, '0');
+	else PrintField(Field, n, 'X');*/
+	PrintField(Field, n, Player);
+}
+void Check(char Field[], const int n, char Player)
+{
+	bool game_over = false;
+	 if (Field[0] == Field[4] && Field[4] == Field[8] && Field[0] != 0) game_over = true; //по диагонали
+else if (Field[2] == Field[4] && Field[4] == Field[6] && Field[2] != 0) game_over = true;
+
+else if (Field[0] == Field[1] && Field[1] == Field[2] && Field[2] != 0) game_over = true; // по горизонтали 
+else if (Field[3] == Field[4] && Field[4] == Field[5] && Field[5] != 0) game_over = true;
+else if (Field[6] == Field[7] && Field[7] == Field[8] && Field[8] != 0) game_over = true;
+
+else if (Field[0] == Field[3] && Field[3] == Field[6] && Field[6] != 0) game_over = true;// по вертикали 
+else if (Field[1] == Field[4] && Field[4] == Field[7] && Field[7] != 0) game_over = true;
+else if (Field[2] == Field[5] && Field[5] == Field[8] && Field[8] != 0) game_over = true;
+	 
+	 if (game_over)
+	 {
+		 cout << "Player " << Player << " wins."<< endl;
+		 return;
+	 }
+	 if(Player == 'X')Move(Field, n, '0');
+	 else Move(Field, n, 'X');
 }
